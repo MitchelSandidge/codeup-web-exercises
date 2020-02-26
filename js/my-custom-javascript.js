2306b72c9ca96c7a5f93f83048d6fb83985b30c4
Mitchel Sandidge <masandidge12@gmail.com>
2020-02-26 11:54:26 -0600

    jQuery exercises

---
+++ js/my-custom-javascript.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+$(document).ready(function() {
+
+// $('#big-div').css('background-color', 'grey');
+
+// $('.codeup').css('border', '1px solid black');
+
+$('li').css('font-size', '30px');
+
+$('h1, p, li').css('background-color', 'lightgray');
+
+
+alert($('#big-div').html());
+
+});

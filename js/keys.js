0660f488f685a77e6b32ba4687ef6ee18caff415
Mitchel Sandidge <masandidge12@gmail.com>
2020-03-11 15:21:31 -0500

    commit for promises

--- js/keys.js
+++ js/keys.js
@@ -1,3 +1,7 @@
 var mapboxToken = "pk.eyJ1IjoibWl0Y2hlbHNhbmQiLCJhIjoiY2s2dHR0Y3Z3MDJ3dTNucXN1MW1jNTl3ciJ9.gz06kiCfbnpx3-jl04fnWA";
 
 var darkSkyKey = "b303a712352803b34b9d9a3c9b944039";
+
+const apiKeyNYT = 'sQZY872uLw5DqcINvdCSEQz8fKpUUz9U';
+
+const gitHubAPI = 'f9844dbe624593c2b3871a36a314dfcdc048d6c6';
---
+++ js/promises.js
@@ -0,0 +1,49 @@
+"use strict";
+
+//Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
+
+function wait(num) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, num)
+    });
+
+}
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+//------------------------------
+
+// function wait(num) {
+//     const newPromise = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (num < 4000) {
+//                 resolve();
+//             } else {
+//                 reject();
+//             }
+//         }, num)
+//     });
+//     newPromise.then(() => console.log("resolve/then || Not bad, you only waited " + num/1000 + " seconds"));
+//
+//     newPromise.catch(() => console.log("reject/catch || WOW you're patient, you waited " + num/1000 + " seconds"));
+// }
+//
+//
+// wait(3000);
+// wait(6000);
+
+
+
+
+
+function getGithubUsernames() {
+    return fetch('https://api.github.com/users/MitchelSandidge/events/public', {headers: {'Authorization': 'token '+ gitHubAPI}})
+        .then(response => response.json());
+}
+
+getGithubUsernames().then((data) => {
+    console.log(data);
+    console.log(data[0].payload.commits[0].message);
+}).catch(error => console.error(error));
